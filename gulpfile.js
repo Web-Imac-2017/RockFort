b5a944da54d3c6a6097eb569b9b76f2900850a92
corentindechomet <corentindechomet@gmail.com>
2017-02-15 22:34:40 +0100

    Bootstrap grid (only) + src

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
-gulp.task('styles', function() {
-    gulp.src('./src/css/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/css'));
-});
-
-gulp.task('default',function() {
-    gulp.watch('./src/css/*.scss',['styles']);
-});
-
 var input = 'app/front/scss/**/*.scss';
 var output = 'app/front/css';
 
