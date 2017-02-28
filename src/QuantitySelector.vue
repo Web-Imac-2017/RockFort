2db6cfe83b060fa811b2a17752b6fdc30cd0a615
corentindechomet <corentindechomet@gmail.com>
2017-02-28 18:46:01 +0100

    Page produit structure
    Conception de la structure de la page produit, des produits similaires, du système de commentaire

---
+++ src/QuantitySelector.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="quantitySelector">
+    <div class="col-md-6">
+      <div class="row">
+        <div class="incrButton" @click="increment"><img src="src/assets/imgs/incr.png" width="35%" alt=""></div>
+      </div>
+      <div class="row">
+        <div class="decrButton" @click="decrement"><img src="src/assets/imgs/decr.png" width="35%" alt=""></div>
+      </div>
+    </div>
+    <div class="col-md-6">
+      <div class="quantity">
+        {{ quantity }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return{
+      quantity: 0
+    }
+  },
+  methods: {
+    increment() {
+      this.quantity++
+    },
+    decrement() {
+      if (this.quantity > 0)
+      this.quantity--
+    }
+  }
+}
+</script>
