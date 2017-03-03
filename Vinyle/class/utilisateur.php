97b5bb650de2a6a9d450ab81c59d9b2b1ed84b83
«mkimoko» <mathieu.kbs@gmail.com>
2017-03-03 20:45:34 +0100

    fichier- class-

--- Vinyle/class/utilisateur.php
+++ Vinyle/class/utilisateur.php
@@ -1,9 +1,7 @@
-<?php
-    session_start();
-    include_once ('../connexion/connexion.php');
-
-
-    abstract class Utilisateur {
+<? php
+    include ('../connexion/connexion.php');
+    
+            abstract class Utilisateur {
                 protected $id;
                 protected $identifiant;
                 protected $mdp;
@@ -11,71 +9,49 @@
                 protected $prenom;
                 protected $mail;
                 
-
-            public function __construct($mdp,$nom,$prenom,$mail){
-                $this->mdp = $mdp;
-                $this->nom = $nom;
-                $this->prenom = $prenom;
-                $this->mail = $mail;
+                protected function __construct($identifiant,$mdp,$nom,$prenom,$mail){
+                    this.$identifiant = $identifiant;
+                    this.$mdp = $mdp;
+                    this.$nom = $nom;
+                    this.$prenom = $prenom;
+                    this.$mail = $mail;
+                                        
+                    global $connexion;
+                    $requete = $connexion->prepare("INSERT INTO utilisateur VALUES ('','',?,?,?,?,?,'','','','')");
+                    $requete->execute( array($identifiant,$mdp,$nom,$prenom,$mail) );
+                    
+                    $requete = $connexion->prepare("SELECT MAX(id) FROM utilisateur");
+                    $requete->execute( array() );
+                    this.$id = fetch();
+                    
+                }
+                
+                //Suppression User
+                public function __destruct() {
+                    global $connexion;
+                    $requete = $connexion->prepare("DELETE * FROM utilisateur  WHERE id = ?");
+                    $requete->execute( array(this.$id) );
+                }
+                
+                //Connexion
+                protected function connexion($identifiant,$mdp,$type){
+                    session_start();
+                    global $connexion;
+                    $requete = $connexion->prepare("SELECT * FROM utilisateur  WHERE id = ? AND mdp = ? AND type = ?");
+                    $requete->execute( array(this.$id,$mdp,$type) );
+                    if($user_id != NULL){
+                            return $_SESSION['id'];
+                        }
+                    else{
+                            echo "<br/><b> Mauvais login ou mauvais mot de passe, merci de réessayer. </b>";
+                        }
+                }
+                
+                //Connexion
+                protected function deconnexion($identifiant,$mdp,$type){
+                    unset($_SESSION['user_id']);
+                }
             }
-
-                public function getId(){
-                    return $this->id;
-                }
-
-
-                public function getMdp(){
-                    return $this->mdp;
-                }
-
-                 public function getNom(){
-                    return $this->nom;
-                }
-
-                 public function getPrenom(){
-                    return $this->prenom;
-                }
-
-                 public function getMail(){
-                    return $this->mail;
-                }
-
-                public function setId($id){
-                    $this->id = $id;
-                }
-
-                /* Affecte l'id de l'utilisateur a la session */
-                public function connexion(  $type, $mdp, $mail){
-                    global $bdd;
-                    $requete = $bdd->prepare("SELECT id FROM utilisateur WHERE type = ? AND motDePasse = ? AND mail = ?");
-                    $requete->execute(array($type, $mdp, $mail) );
-                    $resultat = $requete->fetch();
-                    $_SESSION['id'] = $resultat;
-                    print_r($resultat);
-                    return $resultat;
-                }
-
-                /* Détruit la session */
-                public function deconnexion(  $type, $mdp, $mail){
-                    global $bdd;
-                    $requete = $bdd->prepare("SELECT id FROM utilisateur WHERE type = ? AND motDePasse = ? AND mail = ?");
-                    $requete->execute(array($type, sha1($mdp), $mail) );
-                    $resultat = $requete->fetchColumn();
-
-                    if($resultat == $_SESSION['id'])
-                        unset($_SESSION['id']);
-                }
-
-                /* Retourne les données de l'utilisateur en fonction de son id */
-                public function info(){
-                     global $bdd;
-                    $requete = $bdd->prepare("SELECT * FROM utilisateur WHERE id = ?");
-                    $requete->execute(array($_SESSION['id']));
-                    $resultat = $requete->fetchAll();
-                    print_r($resultat);
-                    return $resultat;
-                }
-    }
-
-
+            
 ?>
+
