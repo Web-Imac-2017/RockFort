91b46fd472024dc5b5fbbcfc4f07289c578b9c9c
corentindechomet <corentindechomet@gmail.com>
2017-03-05 22:46:22 +0100

    Page d'accueil correction
    Rectification du bandeau d'abonnement, et de la structure de la page

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,33 +2,38 @@
 	<div>
 		<div class="container">
 			<section id="subMenu">
-				<a href="/store/vinyles">
-					<div class="col-md-4">
-						<h2>Vinyles</h2>
-						<img src="src/assets/imgs/vinyles.jpg" />
-					</div>
-				</a>
-				<a href="/store/platines">
-					<div class="col-md-4">
-						<h2>Platines</h2>
-						<img src="src/assets/imgs/platines.jpg" />
-					</div>
-				</a>
-				<a href="/store/coffrets">
-					<div class="col-md-4">
-						<h2>Coffrets</h2>
-						<img src="src/assets/imgs/coffrets.jpg" />
-					</div>
-				</a>
+				<div class="row">
+					<a href="/store/vinyles">
+						<div class="col-md-4">
+							<h2>Vinyles</h2>
+							<img src="src/assets/imgs/vinyles.jpg" />
+						</div>
+					</a>
+					<a href="/store/platines">
+						<div class="col-md-4">
+							<h2>Platines</h2>
+							<img src="src/assets/imgs/platines.jpg" />
+						</div>
+					</a>
+					<a href="/store/coffrets">
+						<div class="col-md-4">
+							<h2>Coffrets</h2>
+							<img src="src/assets/imgs/coffrets.jpg" />
+						</div>
+					</a>
+				</div>
 			</section>
 		</div>
-			<section id="callToAction">
-				<div class="slogan col-md-6 nopadding">
+		<section id="callToAction">
+			<div class="row nomargin">
+				<div class="slogan col-md-3">
+					<img src="src/assets/imgs/popart.png" />
+				</div>
+				<div class="slogan col-md-3">
 					<div>
 						<h2>L'abonnement <br />Vinyl Store</h2>
 						<p>Les psychologues<br /> le détestent !</p>
 					</div>
-					<img src="src/assets/imgs/popart.png" />
 				</div>
 				<div class="col-md-6">
 					<div class="border" >
@@ -42,32 +47,36 @@
 						</p>
 					</div>
 				</div>
-			</section>
+			</div>
+		</section>
 		<div class="container">
 			<section id="selection">
-				<a href="#">
-					<div class="col-md-4">
-						<img src="src/assets/imgs/vinyles.jpg" />
-						<h2>Sélection Oldies</h2>
-					</div>
-				</a>
-				<a href="#">
-					<div class="col-md-4">
-						<img src="src/assets/imgs/platines.jpg" />
-						<h2>Le coffret du mois</h2>
-					</div>
-				</a>
-				<a href="#">
-					<div class="col-md-4">
-						<img src="src/assets/imgs/coffrets.jpg" />
-						<h2>Sélection Nouveautés</h2>
-					</div>
-				</a>
+				<div class="row">
+					<a href="#">
+						<div class="col-md-4">
+							<img src="src/assets/imgs/vinyles.jpg" />
+							<h2>Sélection Oldies</h2>
+						</div>
+					</a>
+					<a href="#">
+						<div class="col-md-4">
+							<img src="src/assets/imgs/platines.jpg" />
+							<h2>Le coffret du mois</h2>
+						</div>
+					</a>
+					<a href="#">
+						<div class="col-md-4">
+							<img src="src/assets/imgs/coffrets.jpg" />
+							<h2>Sélection Nouveautés</h2>
+						</div>
+					</a>
+				</div>
 			</section>
 		</div>
-			<section id="originalSoundTrack">
-				<a href="#">
+		<section id="originalSoundTrack">
+			<a href="#">
 				<div class="container">
+					<div class="row">
 						<div class="col-md-4">
 							<img src="src/assets/imgs/vinyles.jpg" />
 						</div>
@@ -79,14 +88,15 @@
 							<img src="src/assets/imgs/ost.jpg" />
 							<p>Retrouvez les plus beaux moments du cinéma sur vos platines !</p>
 						</div>
-				</div>
+					</div>
 				</a>
-			</section>
+			</div>
+		</section>
 	</div>
 </template>
 
 <script>
-	export default {
+export default {
 
-	}
+}
 </script>
