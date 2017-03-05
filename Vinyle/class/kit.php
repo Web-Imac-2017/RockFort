8f9ce92432f682569eeb4a75d2fe9ad199c5240b
«mkimoko» <mathieu.kbs@gmail.com>
2017-03-05 21:49:19 +0100

    Les fichiers valide du back

---
+++ Vinyle/class/kit.php
@@ -0,0 +1,29 @@
+<?php
+    include 'produit.php';
+
+    class Kit extends Produit{
+        function __construct($nom,$image,$musique,$prix,$tag,$artiste,$description) {
+            Produit::__construct($nom,$image,$musique,$prix,$tag,$artiste,$description);
+            $this->type = "Kit";
+        }
+
+        public function ajoutBddKit(){
+        	Produit::ajoutBdd("Album");
+    	}
+	}
+
+    $nom = "This time around";
+	$image = "image";
+	$musique = "musique";
+	$prix = 99.99;
+	$tag = array('a' => "KingOfPop", 'b' => "EastCoast", 'c' => "BigPopa", 'd' => "BadBoyRecords");
+	$mj = new Artiste("Michael Jackson", "Artiste de varieté le plus courroner de tous les temps", "image");
+	$biggie = new Artiste("Notorious BIG", "la star du label Bad Boy", "image");
+	$artiste = array('a' => $mj, 'b' => $biggie);
+	$description = "Un des deux morceaux où le KingOfPop collabore avec la star du label Bad Boy ";
+
+	$vinyle = new Kit($nom,$image,$musique,$prix,$tag,$artiste,$description);
+	$vinyle->ajoutBddKit();
+
+
+?>
