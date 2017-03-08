3efc84255a51490391088abb26d229a4cf2cfcef
AlexaneLG <leguern.a@laposte.net>
2017-03-08 21:55:44 +0100

    Modification méthodes de la classe Adresse

--- Vinyle/class/adresse.php
+++ Vinyle/class/adresse.php
@@ -1,47 +1,95 @@
 <?php
-    include '../connexion/connexion.php';
-   /*********************************ADRESSE*********************************/
+include '../connexion/connexion.php';
+include 'client.php';
 
-    class Adresse{
-        private $numero;
-        private $rue;
-        private $code_postale;
-        private $ville;
-        private $region;
-        private $pays;
-        
-        public function __construct($numero,$rue,$code_postale,$ville,$region,$pays){
-            $this->$numero = $numero;
-            $this->$rue = $rue;
-            $this->$code_postale = $code_postale;
-            $this->$ville = $ville;
-            $this->$region = $region;
-            $this->$pays = $pays;
-        } 
-        
+/*********************************ADRESSE*********************************/
 
-        public function getNumero(){
-            return $this->numero;
-        }
+class Adresse{
+    private $id;
+    private $id_commande;
+    private $adresse;
+    private $ville;
+    private $region;
+    private $pays;
 
-        public function getRue(){
-            return $this->rue;
-        }
-
-        public function getCode_Postale(){
-            return $this->code_postale;
-        }
-
-        public function getVille(){
-            return $this->ville;
-        }
-
-        public function getRegion(){
-            return $this->region;
-        }
-
-        public function getPays(){
-            return $this->pays;
-        }
+    public function __construct($id_commande,$adresse,$ville,$region,$pays){
+        $this->$id_commande = $id_commande;
+        $this->$adresse = $adresse;
+        $this->$ville = $ville;
+        $this->$region = $region;
+        $this->$pays = $pays;
     }
+
+    public function getIdCommande(){
+        return $this->id_commande;
+    }
+
+    public function getAdresse(){
+        return $this->adresse;
+    }
+
+    public function getVille(){
+        return $this->ville;
+    }
+
+    public function getRegion(){
+        return $this->region;
+    }
+
+    public function getPays(){
+        return $this->pays;
+    }
+
+    public function ajoutBdd(){
+        global $bdd;
+
+        $requete = $bdd->prepare("SELECT id FROM adresse WHERE id_commande = ?");
+
+        $requete->execute( array( $this->getIdCommande() ) ); //récupérer id de la commande du client
+        $resultat = $requete->fetchColumn();
+
+        if($resultat != NULL)
+            echo "Une adresse est déjà associée à cette commande.";
+
+        else{
+            $requete = $bdd->prepare("INSERT INTO adresse (id_commande,adresse,ville,region,pays) VALUES (?,?,?,?,?)");
+            $requete->execute( array($this->getIdCommande(),$this->getAdresse(),$this->getVille(),$this->getRegion(),$this->getPays() ) );
+        }
+
+    }
+
+    public function majBdd(){
+        global $bdd;
+
+        $requete = $bdd->prepare("SELECT id FROM adresse WHERE id_commande = ?");
+
+        $requete->execute( array( $this->getIdCommande() ) ); //récupérer id de la commande du client
+        $resultat = $requete->fetchColumn();
+
+        if($resultat != NULL){
+            $requete = $bdd->prepare("UPDATE adresse SET adresse=?,ville=?,region=?,pays=? WHERE id_commande=?");
+            $requete->execute( array($this->getAdresse(),$this->getVille(),$this->getRegion(),$this->getPays(),$this->getIdCommande() ) );
+        }else{
+            echo "Pas d'adresse à remplacer.";
+        }
+
+    }
+
+    public function supprBdd(){
+        global $bdd;
+
+        $requete = $bdd->prepare("SELECT id FROM adresse WHERE id_commande = ?");
+
+        $requete->execute( array( $this->getIdCommande() ) ); //récupérer id de la commande du client
+        $resultat = $requete->fetchColumn();
+
+        if($resultat != NULL){
+            $requete = $bdd->prepare("DELETE FROM adresse WHERE id_commande=?");
+            $requete->execute( array($this->getIdCommande()) );
+        }else{
+            echo "Pas d'adresse à supprimer.";
+        }
+
+    }
+}
 ?>
