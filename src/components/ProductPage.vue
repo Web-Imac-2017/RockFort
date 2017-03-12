1b4d0d94d99ed4ce2e39f4b62ed626a1032a98ab
Henri Respaud-Bouny <henrirespaudbouny@gmail.com>
2017-03-12 10:22:15 +0100

    ProductPage : Ajout du scroll infini sur les commentaires

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -26,11 +26,10 @@
 										<QuantitySelector></QuantitySelector>
 									</div>
 									<div class="col-md-8">
-										<div class="addToCart">
-											Ajouter au panier
-										</div>
-									</div>
-								</div>
+								        <button class="addToCart" @click="addToCart">
+								           	Ajouter au panier
+								        </button>
+								    </div>
 								<div class="row">
 									<div class="col-md-8 col-md-offset-4">
 										<div class="deliveryInfos">Livraison sous 4 jours</div>
@@ -76,57 +75,20 @@
 									<p class="note">Donner mon avis</p>
 								</div>
 							</div>
-							<div class="comment">
+							<div v-for="comment in selectComments(commentLimit)" class="comment">
 								<div class="row mb">
 									<div class="col-md-3">
 										<div class="commentInfo">
-											<p class="author">Michel L.</p>
-											<p class="date">le 12 Fév. 2017</p>
+											<p class="author">{{comment.auteur}}</p>
+											<p class="date">le {{comment.date}}</p>
 										</div>
 									</div>
 									<div class="col-md-9">
 										<div class="commentContent">
-											<p class="commentTitle">Un classique !</p>
-											<p class="commentDesc">Que du bon sur cete album. Ça fait du bien de ré-écouter ces classiques !</p>
+											<p class="commentTitle">{{comment.titre}}</p>
+											<p class="commentDesc">{{comment.texte}}</p>
 										</div>
 									</div>
-								</div>
-							</div>
-							<div class="comment">
-								<div class="row mb">
-									<div class="col-md-3">
-										<div class="commentInfo">
-											<p class="author">Michel L.</p>
-											<p class="date">le 12 Fév. 2017</p>
-										</div>
-									</div>
-									<div class="col-md-9">
-										<div class="commentContent">
-											<p class="commentTitle">Un classique !</p>
-											<p class="commentDesc">Que du bon sur cete album. Ça fait du bien de ré-écouter ces classiques !</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="comment">
-								<div class="row mb">
-									<div class="col-md-3">
-										<div class="commentInfo">
-											<p class="author">Michel L.</p>
-											<p class="date">le 12 Fév. 2017</p>
-										</div>
-									</div>
-									<div class="col-md-9">
-										<div class="commentContent">
-											<p class="commentTitle">Un classique !</p>
-											<p class="commentDesc">Que du bon sur cete album. Ça fait du bien de ré-écouter ces classiques !</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-md-3 col-md-offset-9">
-									<p class="moreNote">Plus d'avis</p>
 								</div>
 							</div>
 						</div>
@@ -138,13 +100,16 @@
 </template>
 
 <script>
+import CartState from '../shoppingCartState'
 import QuantitySelector from '../QuantitySelector.vue'
 
 export default{
 	components : { QuantitySelector },
 	data () {
 		return{
-			products: []
+			products: [],
+			commentaires: [],
+			commentLimit:3
 		}
 	},
 	mounted () {
@@ -154,13 +119,26 @@ export default{
 	    }, (response) => {
 	      console.log("erreur", response)
 	    })
+	    this.$http.get('/src/jsonTestCommentaire.json').then((response) => {
+	      console.log("success", response)
+	      this.commentaires = response.data
+	    }, (response) => {
+	      console.log("erreur", response)
+	    })
+  	},
+  	created() {
+  		window.addEventListener('scroll', this.handleScroll)
   	},
   	methods:{
+  		addToCart() {
+      		CartState.add(this.products[parseInt(window.location.pathname.split('/').pop())-1])
+	    },
   		selectProduct(limit, update){
   			var resultatsArray = this.products;
   			var count = 0;
 
   			if(update == true){
+  				console.log('lol')
           		this.$forceUpdate();
         	}
 
@@ -172,24 +150,38 @@ export default{
   				return resultatsArray;
   			}
 
-  			function shuffle(a) {
-          		for (let i = a.length; i; i--) {
-            		let j = Math.floor(Math.random() * i);
-            		[a[i - 1], a[j]] = [a[j], a[i - 1]];
-          		}
-        	}
-        	shuffle(resultatsArray);
-
         	resultatsArray = resultatsArray.filter(function(item){
-	          	if(item.id != window.location.pathname.split('/').pop() && count < limit) {
+        		console.log('looool')
+	          	if(item.id != window.location.pathname.split('/').pop() && item.genre.indexOf('coupdecoeur') !== -1 && count < limit) {
+	          		console.log('loodqqdool')
 	            	count++
 	            	return item
 	          	}
         	})
         	return resultatsArray
-  		}
+  		},
+  		selectComments(limit){
+  			var resultatsArray = this.commentaires;
+  			var count = 0;
+
+  			resultatsArray = resultatsArray.filter(function(item){
+	          	if(count < limit) {
+	            	count++
+	            	return item
+	          	}
+        	})
+        	return resultatsArray
+
+  		},
+  		handleScroll() {
+  			console.log(document.documentElement.scrollHeight - document.documentElement.clientHeight)
+    		var currentScrollPosition = window.scrollY;
+            console.log("Scrolling down"+ currentScrollPosition);
+            if(currentScrollPosition >= document.documentElement.scrollHeight - document.documentElement.clientHeight){
+            	this.commentLimit += 3
+            }
+   		}
 
   	}
-
 }
 </script>
