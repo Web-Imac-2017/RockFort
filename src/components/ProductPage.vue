59e7dc170b70b79e2bf3fe91df1cb6c10cf5209b
corentindechomet <corentindechomet@gmail.com>
2017-03-13 21:31:48 +0100

    div non fermées

--- src/components/ProductPage.vue
+++ src/components/ProductPage.vue
@@ -3,7 +3,7 @@
 		<div id="productPage" >
 			<div class="container">
 				<div class="row">
-						<div v-for="product in selectProduct(1, false)" class="col-md-8 col-md-offset-2">
+					<div v-for="product in selectProduct(1, false)" class="col-md-8 col-md-offset-2">
 						<div class="row">
 							<div class="col-md-6">
 								<img class="vinyleImg" :src="product.image" />
@@ -26,76 +26,77 @@
 										<QuantitySelector></QuantitySelector>
 									</div>
 									<div class="col-md-8">
-								        <button class="addToCart" @click="addToCart">
-								           	Ajouter au panier
-								        </button>
-								    </div>
-								<div class="row">
-									<div class="col-md-8 col-md-offset-4">
-										<div class="deliveryInfos">Livraison sous 4 jours</div>
+										<button class="addToCart" @click="addToCart">
+											Ajouter au panier
+										</button>
 									</div>
-								</div>
-								<div class="row">
-									<div class="col-md-12">
-										<div class="musicPlayer">
-											Ecouter un extrait
+									<div class="row">
+										<div class="col-md-8 col-md-offset-4">
+											<div class="deliveryInfos">Livraison sous 4 jours</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-md-12">
+											<div class="musicPlayer">
+												Ecouter un extrait
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<div class="desc">
-									{{product.description}}
+							<div class="row">
+								<div class="col-md-12">
+									<div class="desc">
+										{{product.description}}
+									</div>
 								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12">
-								<p class="more">Vous aimerez aussi</p>
+							<div class="row">
+								<div class="col-md-12">
+									<p class="more">Vous aimerez aussi</p>
+								</div>
 							</div>
-						</div>
-						<div class="row">
-							<div v-for="product in selectProduct(4, false)" class="col-md-3">
-								<div @click="selectProduct(1,true)" class="vinyleItem">
-									<router-link :to="{ name: 'product', params: { type:product.type , id: product.id }}"><img :src="product.image" /></router-link>
+							<div class="row">
+								<div v-for="product in selectProduct(4, false)" class="col-md-3">
+									<div @click="selectProduct(1,true)" class="vinyleItem">
+										<router-link :to="{ name: 'product', params: { type:product.type , id: product.id }}"><img :src="product.image" /></router-link>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div class="ratingArea">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-8 col-md-offset-2">
-							<div class="row">
-								<div class="col-md-3" v-on:click="commentFormToggle()">
-									<p v-if="isActive" class="note">Donner mon avis</p>
-									<p v-else class="note">Annuler</p>
-								</div>
-							</div>
-							<div class=" row commentForm" v-bind:class="{ commentFormHidden: isActive }">
-									<form>
-											<input class="col-md-2"  type="text" placeholer="Nom"/>
-											<input class="col-md-push-1 col-md-9" type="text" placeholer="Titre"/>
-											<textarea class="col-md-12" rows="6"></textarea>
-											<button>ENVOYER</button>
-									</form>
-							</div>
-							<div v-for="comment in selectComments(commentLimit)" class="comment">
-								<div class="row mb">
-									<div class="col-md-3">
-										<div class="commentInfo">
-											<p class="author">{{comment.auteur}}</p>
-											<p class="date">le {{comment.date}}</p>
-										</div>
+				<div class="ratingArea">
+					<div class="container">
+						<div class="row">
+							<div class="col-md-8 col-md-offset-2">
+								<div class="row">
+									<div class="col-md-3" v-on:click="commentFormToggle()">
+										<p v-if="isActive" class="note">Donner mon avis</p>
+										<p v-else class="note">Annuler</p>
 									</div>
-									<div class="col-md-9">
-										<div class="commentContent">
-											<p class="commentTitle">{{comment.titre}}</p>
-											<p class="commentDesc">{{comment.texte}}</p>
+								</div>
+								<div class=" row commentForm" v-bind:class="{ commentFormHidden: isActive }">
+									<form>
+										<input class="col-md-2"  type="text" placeholer="Nom"/>
+										<input class="col-md-push-1 col-md-9" type="text" placeholer="Titre"/>
+										<textarea class="col-md-12" rows="6"></textarea>
+										<button>ENVOYER</button>
+									</form>
+								</div>
+								<div v-for="comment in selectComments(commentLimit)" class="comment">
+									<div class="row mb">
+										<div class="col-md-3">
+											<div class="commentInfo">
+												<p class="author">{{comment.auteur}}</p>
+												<p class="date">le {{comment.date}}</p>
+											</div>
+										</div>
+										<div class="col-md-9">
+											<div class="commentContent">
+												<p class="commentTitle">{{comment.titre}}</p>
+												<p class="commentDesc">{{comment.texte}}</p>
+											</div>
 										</div>
 									</div>
 								</div>
@@ -123,83 +124,83 @@ export default{
 		}
 	},
 	mounted () {
-	    this.$http.get('/src/jsonTest.json').then((response) => {
-	      console.log("success", response)
-	      this.products = response.data
-	    }, (response) => {
-	      console.log("erreur", response)
-	    })
-	    this.$http.get('/src/jsonTestCommentaire.json').then((response) => {
-	      console.log("success", response)
-	      this.commentaires = response.data
-	    }, (response) => {
-	      console.log("erreur", response)
-	    })
-  	},
-  	created() {
-  		window.addEventListener('scroll', this.handleScroll)
-  	},
-  	methods:{
-  		addToCart() {
-      		CartState.add(this.products[parseInt(window.location.pathname.split('/').pop())-1])
-	    },
-  		selectProduct(limit, update){
-  			var resultatsArray = this.products;
-  			var count = 0;
+		this.$http.get('/src/jsonTest.json').then((response) => {
+			console.log("success", response)
+			this.products = response.data
+		}, (response) => {
+			console.log("erreur", response)
+		})
+		this.$http.get('/src/jsonTestCommentaire.json').then((response) => {
+			console.log("success", response)
+			this.commentaires = response.data
+		}, (response) => {
+			console.log("erreur", response)
+		})
+	},
+	created() {
+		window.addEventListener('scroll', this.handleScroll)
+	},
+	methods:{
+		addToCart() {
+			CartState.add(this.products[parseInt(window.location.pathname.split('/').pop())-1])
+		},
+		selectProduct(limit, update){
+			var resultatsArray = this.products;
+			var count = 0;
 
-  			if(update == true){
-  				console.log('lol')
-          		this.$forceUpdate();
-        	}
+			if(update == true){
+				console.log('lol')
+				this.$forceUpdate();
+			}
 
-  			if(limit == 1){
-  				resultatsArray = resultatsArray.filter(function(item){
-  					if(item.id == window.location.pathname.split('/').pop())
-  						return item
-  				})
-  				return resultatsArray;
-  			}
+			if(limit == 1){
+				resultatsArray = resultatsArray.filter(function(item){
+					if(item.id == window.location.pathname.split('/').pop())
+					return item
+				})
+				return resultatsArray;
+			}
 
-        	resultatsArray = resultatsArray.filter(function(item){
-        		console.log('looool')
-	          	if(item.id != window.location.pathname.split('/').pop() && item.genre.indexOf('coupdecoeur') !== -1 && count < limit) {
-	          		console.log('loodqqdool')
-	            	count++
-	            	return item
-	          	}
-        	})
-        	return resultatsArray
-  		},
-  		commentFormToggle() {
-	      console.log(!this.isActive)
-	      this.isActive = !this.isActive;
-	      return this.isActive;
-    	},
-  		selectComments(limit){
-  			var resultatsArray = this.commentaires;
-  			var count = 0;
+			resultatsArray = resultatsArray.filter(function(item){
+				console.log('looool')
+				if(item.id != window.location.pathname.split('/').pop() && item.genre.indexOf('coupdecoeur') !== -1 && count < limit) {
+					console.log('loodqqdool')
+					count++
+					return item
+				}
+			})
+			return resultatsArray
+		},
+		commentFormToggle() {
+			console.log(!this.isActive)
+			this.isActive = !this.isActive;
+			return this.isActive;
+		},
+		selectComments(limit){
+			var resultatsArray = this.commentaires;
+			var count = 0;
 
-  			resultatsArray = resultatsArray.filter(function(item){
-	          	if(count < limit) {
-	            	count++
-	            	return item
-	          	}
-        	})
-        	return resultatsArray
+			resultatsArray = resultatsArray.filter(function(item){
+				if(count < limit) {
+					count++
+					return item
+				}
+			})
+			return resultatsArray
 
-  		},
-  		handleScroll() {
-  			console.log(document.documentElement.scrollHeight - document.documentElement.clientHeight)
-    		var currentScrollPosition = window.scrollY;
-            console.log("Scrolling down"+ currentScrollPosition);
-            if(currentScrollPosition >= document.documentElement.scrollHeight - document.documentElement.clientHeight){
-            	this.commentLimit += 3
-            }
-   		}
+		},
+		handleScroll() {
+			console.log(document.documentElement.scrollHeight - document.documentElement.clientHeight)
+			var currentScrollPosition = window.scrollY;
+			console.log("Scrolling down"+ currentScrollPosition);
+			if(currentScrollPosition >= document.documentElement.scrollHeight - document.documentElement.clientHeight){
+				this.commentLimit += 3
+			}
+		}
 
-  	},
-  	destroyed () {
-  		window.removeEventListener('scroll', this.handleScroll);
+	},
+	destroyed () {
+		window.removeEventListener('scroll', this.handleScroll);
 	}
 }
 </script>
