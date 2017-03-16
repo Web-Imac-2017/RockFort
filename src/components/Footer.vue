e294b34b80e13009e66508f19a5325538a2a8735
corentindechomet <corentindechomet@gmail.com>
2017-03-16 17:01:10 +0100

    Nouvelle version

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,28 +5,43 @@
         <div class="col-xs-4">
           <div class="about">
             <ul>
-              <li><a href="#">Contact</a></li>
-              <li><a href="#">L'équipe</a></li>
-              <li><a href="#">Mentions légales</a></li>
+              <li>
+                <router-link to="/contact">
+                  Contact
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/lequipe">
+                  L'équipe
+                </router-link>
+              </li>
+              <li>
+                <router-link to="/mentionslegales">
+                  Mentions légales
+                </router-link>
+              </li>
             </ul>
           </div>
         </div>
         <div class="col-xs-5 col-xs-offset-3">
           <div class="social">
             <ul>
-              <li><a href="#"><img src="/src/assets/imgs/facebook.png" /></a></li>
-              <li><a href="#"><img src="/src/assets/imgs/instagram.png" /></a></li>
-              <li><a href="#"><img src="/src/assets/imgs/twitter.png" /></a></li>
-            </ul>
+              <li>
+                <img src="/src/assets/imgs/facebook.png" /></a></li>
+                <li>
+                  <img src="/src/assets/imgs/instagram.png" /></a></li>
+                  <li>
+                    <img src="/src/assets/imgs/twitter.png" /></a></li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
+          <!-- <div class="col-xs-2 col-xs-offset-2">
+          <div class="newsletter">
+          <p>La Newsletter</p>
+          <input type="text" placeholder="Entrez votre email" />
         </div>
-      </div>
-    </div>
-    <!-- <div class="col-xs-2 col-xs-offset-2">
-    <div class="newsletter">
-    <p>La Newsletter</p>
-    <input type="text" placeholder="Entrez votre email" />
-  </div>
-</div> -->
-</footer>
-</template>
+      </div> -->
+    </footer>
+  </template>
